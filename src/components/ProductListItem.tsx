8b17ceb6ac726c7c1717e1c4a199252ca9779d8c
erikheuna <charlesricheuna@gmail.com>
2025-02-05 01:39:22 +0100

    First custom component created.

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet, View, Image, Text } from "react-native";
+import Colors from "../constants/Colors";
+
+const ProductListItem = ({product}: {product: any}) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+});
+
+export default ProductListItem;
