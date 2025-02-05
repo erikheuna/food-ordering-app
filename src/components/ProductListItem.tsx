4e12ed5d1c14650d612864e0ede38718c062535b
erikheuna <charlesricheuna@gmail.com>
2025-02-05 02:13:00 +0100

    Product List initialized.

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,10 +1,22 @@
 import { StyleSheet, View, Image, Text } from "react-native";
-import Colors from "../constants/Colors";
+import Colors from "@/constants/Colors";
+import { Product } from "@/types";
 
-const ProductListItem = ({product}: {product: any}) => {
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -16,6 +28,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   image: {
     width: "100%",
