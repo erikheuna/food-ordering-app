10485012acea288390b81aa5f5caaed7148c5744
erikheuna <charlesricheuna@gmail.com>
2025-02-08 00:15:39 +0100

    ShoppingCart done.

---
+++ src/app/cart.tsx
@@ -0,0 +1,28 @@
+import { FlatList, Platform, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { CartContext, useCart } from "@/providers/CartProvider";
+import CartListItem from "@/components/CartListItem";
+import Button from "@/components/Button";
+
+const CartScreen = () => {
+  const { items, total } = useCart();
+
+  return (
+    <View style={{ padding: 10 }}>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        contentContainerStyle={{ padding: 10, gap: 10}}
+      />
+      
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: '500' }}>Total: ${total}</Text>
+      <Button text="Checkout" />
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+    </View>
+  );
+};
+
+export default CartScreen;
+
+const styles = StyleSheet.create({});
