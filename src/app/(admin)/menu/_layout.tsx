138a838e5ec29b4a44167a4bf8719ab4b8e9cae8
erikheuna <charlesricheuna@gmail.com>
2025-02-08 13:34:07 +0100

    CreateProduct form done.

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -12,7 +12,7 @@ export default function MenuStack() {
           title: "Menu",
           headerTitleAlign: "center",
           headerRight: () => (
-            <Link href="/" asChild>
+            <Link href="/(admin)/menu/create" asChild>
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
