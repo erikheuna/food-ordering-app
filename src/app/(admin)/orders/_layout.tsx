e6c042b2fe41b39cc6493dd4cad6b7f63e667a89
erikheuna <charlesricheuna@gmail.com>
2025-02-09 17:32:30 +0100

    Top Tab Navigator in admin side done.

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function OrdersLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="list" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
